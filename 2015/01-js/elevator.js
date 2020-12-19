dcd55a5f1db059a6a91691a905655b6a50e5b465
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2020-12-19 13:37:13 +0100

    2015.01.2

--- 2015/01-js/elevator.js
+++ 2015/01-js/elevator.js
@@ -2,7 +2,18 @@ const instructions = (text) => text.trim().split("");
 
 const run = (is) => is.reduce((p, n) => p + (n === "(" ? 1 : -1), 0);
 
+const enterLevel = (is, level) => {
+    let currentLevel = 0;
+    let index = 0;
+    while (currentLevel !== level) {
+        if (currentLevel === undefined) currentLevel = 0;
+        currentLevel += is[index++] === "(" ? 1 : -1;
+    }
+    return index;
+}
+
 module.exports = {
     instructions,
-    run
+    run,
+    enterLevel
 }
--- 2015/01-js/index.js
+++ 2015/01-js/index.js
@@ -1,4 +1,4 @@
-const { instructions, run } = require("./elevator");
+const { instructions, run, enterLevel } = require("./elevator");
 const fs = require("fs").promises;
 const path = require("path");
 
@@ -7,6 +7,8 @@ const path = require("path");
         const data = await fs.readFile(`${path.dirname(require.main.filename)}/input.txt`, "utf8");
         const is = instructions(data);
         console.log(`${run(is)}`);
+
+        console.log(`${enterLevel( is, -1) }`)
 
     } catch (err) {
         console.log(err)
