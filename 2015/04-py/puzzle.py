874fbabfbbbc0f4186c712ed2e9dbdc7cdcf0de1
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2020-12-20 23:17:48 +0100

    2015.04.1

---
+++ 2015/04-py/puzzle.py
@@ -0,0 +1,21 @@
+import hashlib
+
+
+def advent_coin(seed):
+    counter = 0
+    while True:
+        to_be_coded = seed + str(counter)
+        yield hashlib.md5(to_be_coded.encode()).hexdigest(), counter
+        counter += 1
+
+
+def main():
+    input = "yzbqklnj"
+    for code, counter in advent_coin(input):
+        if code[:5] == "00000":
+            print(code, counter)
+            break
+
+
+if __name__ == "__main__":
+    main()
