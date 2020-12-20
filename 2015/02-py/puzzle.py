07aab1332fc251de63ab304b012af01e60bf1c45
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2020-12-20 19:44:20 +0100

    program structure

--- 2015/02-py/puzzle.py
+++ 2015/02-py/puzzle.py
@@ -2,12 +2,32 @@ import os
 from functools import reduce
 
 
+
+def parse_gifts(text):
+    pass
+
+def total_of(gifts, f):
+    pass
+
+def paper(gift):
+    pass
+
+def ribbon(gift):
+    pass
+
 def main():
     file_path = "./input.txt" if len(os.path.dirname(__file__)) == 0 else os.path.dirname(__file__) + "/input.txt"
 
     with open(file_path, 'r') as file:
-        path = file.read()
+        gift_description = file.read()
 
+    gifts = parse_gifts(gift_description)
+
+    tot_paper = total_of(gifts, paper)
+    tot_ribbon = total_of(gifts, ribbon)
+
+    print(tot_paper)
+    print(tot_ribbon)
 
 if __name__ == "__main__":
     main()
