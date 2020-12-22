84e9b7b43dd1e53e099aec33572d998a58311665
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2020-12-22 20:45:47 +0100

    2015.06.1

--- 2015/06-py/puzzle.py
+++ 2015/06-py/puzzle.py
@@ -3,43 +3,23 @@ import re
 
 
 def main():
-    # coding=utf8
-    # the above tag defines encoding for this document and is for Python 2.x compatibility
-
-
-
-    regex = r"(on|off|toggle) ([0-9]+,[0-9]+) through ([0-9]+,[0-9]+)"
-
-    test_str = ("turn off 301,3 through 808,453\n"
-                "turn on 351,678 through 951,908\n"
-                "toggle 720,196 through 897,994")
-
-    matches = re.finditer(regex, test_str, re.MULTILINE)
-
-    for matchNum, match in enumerate(matches, start=1):
-
-        print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
-                                                                            end=match.end(), match=match.group()))
-
-        for groupNum in range(0, len(match.groups())):
-            groupNum = groupNum + 1
-
-            print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum=groupNum,
-                                                                            start=match.start(groupNum),
-                                                                            end=match.end(groupNum),
-                                                                            group=match.group(groupNum)))
-
-    # Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
-
-    return
+    lights = [[False for col in range(1000)] for row in range(1000)]
     with open("./input.txt" if len(os.path.dirname(__file__)) == 0 else os.path.dirname(__file__) + "/input.txt",
               'r') as file:
+        count_lit = 0;
         for line in file:
-            matches = re.findall(r"(on|off|toggle) ([0-9]+,[0-9]+)", line, re.MULTILINE)
-            print(f"|{matches}|")
-
-
-
+            matches = re.findall(r"(on|off|toggle) (\d+),(\d+) through (\d+),(\d+)", line)[0]
+            for row in range(int(matches[1]), int(matches[3]) + 1):
+                for col in range(int(matches[2]), int(matches[4]) + 1):
+                    prev = lights[row][col]
+                    if matches[0] == "on":
+                        lights[row][col] = True
+                    elif matches[0] == "off":
+                        lights[row][col] = False
+                    else:
+                        lights[row][col] = not lights[row][col]
+                    count_lit += 1 if not prev and lights[row][col] else -1 if prev and not lights[row][col] else 0
+        print(count_lit)
 
 
 if __name__ == "__main__":
