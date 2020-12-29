6387cd6dcc588a6f6eb822ee1ffc785812577575
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2020-12-29 23:11:31 +0100

    2015.10.2

--- 2015/10-py/puzzle.py
+++ 2015/10-py/puzzle.py
@@ -1,19 +1,16 @@
-def main():
-    _input_text = "1"
-    input_text = "1113222113"
-    for i in range(40):
-        index = 0
-        new_input = ""
-        while index < len(input_text):
-            count = 1
-            while (index < len(input_text) - 1) and (input_text[index] == input_text[index + 1]):
-                index += 1
-                count += 1
-            new_input += str(count) + input_text[index]
+_input_text = "1"
+input_text = "1113222113"
+for i in range(50):
+    index = 0
+    new_input = ""
+    while index < len(input_text):
+        count = 1
+        while (index < len(input_text) - 1) and (input_text[index] == input_text[index + 1]):
             index += 1
-        input_text = new_input
-        print(len(input_text))
-
-
-if __name__ == "__main__":
-    main()
+            count += 1
+        new_input += str(count) + input_text[index]
+        index += 1
+    input_text = new_input
+    if i == 39:
+        print("step_1", len(input_text))
+print("step_2", len(input_text))
