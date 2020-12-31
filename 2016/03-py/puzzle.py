9fcbcbc34dc277113a509e4b2ef58708e90d863d
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2020-12-31 12:15:24 +0100

    2016.03.2

--- 2016/03-py/puzzle.py
+++ 2016/03-py/puzzle.py
@@ -2,13 +2,26 @@ import os
 import re
 
 
-input_file = "/input.txt"
+def possible(triangle):
+    edges = sorted(triangle)
+    return edges[0] + edges[1] > edges[2]
 
+
+input_file = "/input.txt"
 with open(f".{input_file}" if len(os.path.dirname(__file__)) == 0 else os.path.dirname(__file__) + input_file, 'r') as file:
-    possible_count = 0
+    possible_count_step1 = 0
+    possible_count_step2 = 0
+    lines = []
     for line in file:
         match = re.match(r"[ \t]*(\d*)[ \t]*(\d*)[ \t]*(\d*)", line.strip())
-        edges = sorted(list(map(lambda x: int(x), match.groups())))
-        if edges[0] + edges[1] > edges[2]:
-            possible_count += 1
-    print("step_1", possible_count)
+        line_numbers = list(map(lambda x: int(x), match.groups()))
+        lines.append(line_numbers)
+        if possible(line_numbers):
+            possible_count_step1 += 1
+        if len(lines) == 3:
+            for i in range(3):
+                if possible([lines[0][i], lines[1][i], lines[2][i]]):
+                    possible_count_step2 += 1
+            lines = []
+    print("step_1", possible_count_step1)
+    print("step_2", possible_count_step2)
