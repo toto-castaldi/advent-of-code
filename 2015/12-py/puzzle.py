6b349def13d4743a87291aa071c9d3eb5430dcd2
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2021-01-06 17:47:46 +0100

    2015.12.2

--- 2015/12-py/puzzle.py
+++ 2015/12-py/puzzle.py
@@ -3,22 +3,25 @@ import os
 
 
 def rec_json(j):
-    acc = 0
+    acc_1 = 0
+    acc_2 = 0
     if isinstance(j, list):
         for el in j:
-            acc += rec_json(el)
+            acc_1 += rec_json(el)[0]
+            acc_2 += rec_json(el)[1]
     elif isinstance(j, dict):
+        has_red = "red" in j.values()
         for el in j.values():
-            acc += rec_json(el)
+            acc_1 += rec_json(el)[0]
+            acc_2 += rec_json(el)[1] if not has_red else 0
     elif type(j) == int:
-        acc += j
-    else:
-        print("skip", j)
-    return acc
+        acc_1 += j
+        acc_2 += j
+    return acc_1, acc_2
 
 
 file_name = "/input.json"
 with open(f".{file_name}" if len(os.path.dirname(__file__)) == 0 else os.path.dirname(__file__) + file_name,
           'r') as json_file:
     data = json.load(json_file)
-    print("step_1", rec_json(data))
+    print(rec_json(data))
