3ea141d793d4537721fbf9fd0c4abbc1ceb99617
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2021-01-13 15:26:03 +0100

    2015.16.2

--- 2015/16-py/puzzle.py
+++ 2015/16-py/puzzle.py
@@ -17,10 +17,21 @@ with open(f".{file_name}" if len(os.path.dirname(__file__)) == 0 else os.path.di
         "cars": 2,
         "perfumes": 1
     }
+
+    def valid(field_name, field_value_str):
+        field_value = int(field_value_str)
+        if field_name == "cats" or field_name == "trees":
+            return field_value > the_sue.get(field_name)
+        if field_name == "pomeranians" or field_name == "goldfish":
+            return field_value < the_sue.get(field_name)
+        return the_sue.get(field_name, None) == field_value
+
     for line in file:
         fm = re.fullmatch(r"Sue (\d*): (\w*): (\d*), (\w*): (\d*), (\w*): (\d*)", line.strip())
         sue_num, field_1, value_1, field_2, value_2, field_3, value_3 = fm.groups()
         if the_sue.get(field_1, None) == int(value_1) and the_sue.get(field_2, None) == int(value_2) and the_sue.get(field_3, None) == int(value_3):
-            print(sue_num)
+            print("step_1", sue_num)
+        if valid(field_1, value_1) and valid(field_2, value_2) and valid(field_3, value_3):
+            print("step_2", sue_num)
 
 
