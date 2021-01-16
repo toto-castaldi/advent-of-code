7dfb63c6f5340438a03c20d53adf14572eae296b
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2021-01-16 11:16:19 +0100

    2016.06.2

--- 2016/06-py/puzzle.py
+++ 2016/06-py/puzzle.py
@@ -12,10 +12,13 @@ def main():
                     common_chars.append({})
                 common_char = common_chars[index]
                 common_char[c] = common_char.get(c, 0) + 1
-        error_corrected = []
+        error_corrected_step1 = []
+        error_corrected_step2 = []
         for common_char in common_chars:
-            error_corrected.append(sorted(common_char.items(), reverse=True, key=lambda item: item[1])[0][0])
-        print(''.join(error_corrected))
+            error_corrected_step1.append(sorted(common_char.items(), reverse=True, key=lambda item: item[1])[0][0])
+            error_corrected_step2.append(sorted(common_char.items(), reverse=False, key=lambda item: item[1])[0][0])
+        print("step_1", ''.join(error_corrected_step1))
+        print("step_2", ''.join(error_corrected_step2))
 
 
 if __name__ == "__main__":
