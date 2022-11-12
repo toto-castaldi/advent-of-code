c283261ed8aba689da70b1c5a30938ee947d2ff6
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-11-12 21:37:33 +0100

    2021.03.2

--- 2021/03/exercise.kt
+++ 2021/03/exercise.kt
@@ -1,23 +1,34 @@
 import java.io.File
 import java.util.Arrays
+import java.util.Collections
+
+fun computeCounters(values: List<String>): Array<Int> {
+    val counters: Array<Int> = Array(values.get(0).length) { 0 }
+    for (it in values) {
+        for (i in 0 until it.length) {
+            counters[i] += it[i].digitToInt()
+        }
+    }
+    return counters;
+}
 
 fun main() {
     val firstLine: String? = File("input.txt").useLines { it.firstOrNull() } 
+    var oxigenValues: List<String> = List(0) { "" }
+    var co2Values: List<String> = List(0) { "" }
+    var values: List<String> = List(0) { "" }
     if (firstLine != null) {
-        var counters: Array<Int> = Array(firstLine.length) { 0 }
-        var lineCount : Int = 0
         File("input.txt").forEachLine { 
-            lineCount ++
-            for (i in 0 until it.length) {
-                counters[i] += it[i].digitToInt()
-            }
+            values = values.plusElement(it)
+            oxigenValues = oxigenValues.plusElement(it)
+            co2Values = co2Values.plusElement(it)
         }
-        println(Arrays.toString(counters))
+        var counters = computeCounters(values);
         var gamma : String = ""
         var epsilon : String = ""
         for (i in 0..counters.size-1) {
             val ones = counters[i]
-            if (lineCount - ones > lineCount / 2) {
+            if (ones >= values.size - ones) {
                 gamma += "1"
                 epsilon += "0"
             } else {
@@ -25,9 +36,39 @@ fun main() {
                 epsilon += "1"
             }
         }
+
+        var i = 0
+        while (oxigenValues.size > 1) {
+            counters = computeCounters(oxigenValues);
+            val ones = counters[i]
+            val zeros = oxigenValues.size - counters[i]
+            if (ones >= zeros) { 
+                oxigenValues = oxigenValues.filter { it[i] == '1' }
+            } else { 
+                oxigenValues = oxigenValues.filter { it[i] == '0' }
+            }
+            i ++
+        }
+
+        i = 0
+        while (co2Values.size > 1) {
+            counters = computeCounters(co2Values);
+            val ones = counters[i]
+            val zeros = co2Values.size - counters[i]
+            if (ones >= zeros) { 
+                co2Values = co2Values.filter { it[i] == '0' }
+            } else { 
+                co2Values = co2Values.filter { it[i] == '1' }
+            }
+            i ++
+        }
+
         val gammaVal : Int = Integer.parseInt(gamma, 2);
         val epsilonVal : Int = Integer.parseInt(epsilon, 2);
-        val result : Int = gammaVal * epsilonVal
-        println("g $gammaVal, e $epsilonVal, res $result")
+        val result1 : Int = gammaVal * epsilonVal
+        val oxigen : Int = Integer.parseInt(oxigenValues[0], 2);
+        val co2 : Int = Integer.parseInt(co2Values[0], 2);
+        val result2 : Int = oxigen * co2
+        println("g $gammaVal, e $epsilonVal, res1 $result1, o $oxigen, co2 $co2, res2 $result2 ")
     }
 }
