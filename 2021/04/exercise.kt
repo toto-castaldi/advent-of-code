94aaa0d2fc6dc7a3b5c4dc0124d5b32ccd7cc418
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-11-13 20:22:03 +0100

    2021.04.2

--- 2021/04/exercise.kt
+++ 2021/04/exercise.kt
@@ -5,6 +5,8 @@ import java.util.Collections
 fun main() {
     val unmarked = {board : List<Int> -> board.fold(0) { acc, value -> acc + (if (value >= 100) 0 else value )} } 
     
+    var firstWinning: String = ""
+    var lastWinning: String = ""
     val lines: List<String> = File("input.txt").readLines()
     val calledNumbers: List<Int> = lines.get(0).split(",").map({it.trim().toInt()})
     var boards: ArrayList<List<Int>> = ArrayList()
@@ -15,21 +17,26 @@ fun main() {
         }
         boards.add(board)
     }
-    calling@ for (called in calledNumbers) {
+    for (called in calledNumbers) {
+        println("calling $called")
         val newBoards : ArrayList<List<Int>> = ArrayList()
         for (board in boards) {
             val newBoard = board.map { if (it == called) it + 100 else it }
-            newBoards.add(newBoard)
+            var winning = ""
             for (hStartingIndex in 0..newBoard.size-1 step 5) {
                 var countH = 0
-                for (h in hStartingIndex..4) {
+                for (h in hStartingIndex..hStartingIndex+4) {
                     countH += if (newBoard.get(h) >= 100) 1 else 0
                 }
                 if (countH >= 5) {
                     val u = unmarked(newBoard)
                     val result = u * called
-                    println("H at $newBoard, c $called,  u $u -> $result")
-                    break@calling
+                    winning = "H at $newBoard, c $called,  u $u -> $result"
+                    println(winning)
+                    if (firstWinning == "") {
+                        firstWinning = winning
+                    }
+                    lastWinning = winning
                 }
             }
             for (vStartingIndex in 0..4) {
@@ -41,14 +48,25 @@ fun main() {
                 if (countV >= 5) {
                     val u = unmarked(newBoard)
                     val result = u * called
-                    println("V at $newBoard, c $called,  u $u -> $result")
-                    break@calling
-                }   
+                    winning = "V at $newBoard, c $called,  u $u -> $result"
+                    println(winning)
+                    if (firstWinning == "") {
+                        firstWinning = winning
+                    }
+                    lastWinning = winning
+                }
             }
-            
+            if (winning == "") {
+                newBoards.add(newBoard)
+            }
         }
         boards = ArrayList()
         boards.addAll(newBoards)
     }
+
+    println("first")
+    println(firstWinning)
+    println("last")
+    println(lastWinning)
 
 }
