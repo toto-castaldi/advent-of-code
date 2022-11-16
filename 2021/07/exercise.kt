57eac7501d6eb78b6c91651556ac1313d637348d
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-11-16 19:01:44 +0100

    is not the mean

--- 2021/07/exercise.kt
+++ 2021/07/exercise.kt
@@ -2,13 +2,28 @@ import java.io.File
 import java.util.Arrays
 import java.util.Collections
 
-fun totalFuel(elements : List<Int>, destination : Int) : Int {
+fun totalFuel1(elements : List<Int>, destination : Int) : Int {
     var result = 0;
     for (e in elements) {
-        println("from $e to $destination")
+        //println("from $e to $destination")
         result += Math.abs(destination - e)
     }
-    println(result)
+    //println(result)
+    return result
+}
+
+fun totalFuel2(elements : List<Int>, destination : Int) : Int {
+    var result = 0;
+    
+    for (e in elements) {
+        var cost = 1
+        val distance = Math.abs(destination - e)
+        for (i in 1..distance) {
+            result += cost
+            cost ++
+        }
+    }
+    //println(result)
     return result
 }
 
@@ -31,8 +46,16 @@ fun main() {
             index2 = index1
         }
         println("$index1, $index2")
-        var res1 = totalFuel(positions, sortedPositions[index1])
-        var res2 = totalFuel(positions, sortedPositions[index2])
+        var res1 = totalFuel1(positions, sortedPositions[index1])
+        var res2 = totalFuel1(positions, sortedPositions[index2])
         println("$res1, $res2")
+
+        val sumPositions = positions.fold(0) { acc, value -> acc + value }
+        val meanPosition : Int = Math.ceil(sumPositions.toDouble() / positions.size.toDouble()).toInt()
+
+        println(meanPosition)
+
+        var resStep2 = totalFuel2(positions, meanPosition)
+        println("$resStep2")
     }
 }
