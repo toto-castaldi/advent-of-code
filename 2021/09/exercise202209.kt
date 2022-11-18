3b5e17d55844de42d9fa8973783f9b51a661e9ed
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-11-18 15:26:40 +0100

    2021.09.1

--- 2021/09/exercise202209.kt
+++ 2021/09/exercise202209.kt
@@ -4,8 +4,8 @@ import java.util.ArrayList
 fun main(args: Array<String>) {
     val fileName : String = args[0]
     val fileInput = File(fileName)
-    var levels : ArrayList<Int> = ArrayList()
-    var size : Int = 0
+    val levels : ArrayList<Int> = ArrayList()
+    var size = 0
     fileInput.forEachLine {
         line ->
 
@@ -20,7 +20,7 @@ fun main(args: Array<String>) {
     }
     println(levels)
 
-    var total : Int = 0
+    var total = 0
     for ((i, value) in levels.iterator().withIndex()) {
         val dx : Int ? = if ((i < levels.size -1) && ((i + 1) % size != 0)) levels[i+1] else null
         val sx : Int ? = if ((i > 0) && (i% size != 0)) levels[i-1] else null
@@ -28,13 +28,21 @@ fun main(args: Array<String>) {
         val up : Int ? = if (i - size >= 0) levels[i-size] else null
 
         if (
-            (value < sx ?: Int.MAX_VALUE) &&
-            (value < dx ?: Int.MAX_VALUE) &&
-            (value < dw ?: Int.MAX_VALUE) &&
-            (value < up ?: Int.MAX_VALUE)
+            (value < (sx ?: Int.MAX_VALUE)) &&
+            (value < (dx ?: Int.MAX_VALUE)) &&
+            (value < (dw ?: Int.MAX_VALUE)) &&
+            (value < (up ?: Int.MAX_VALUE))
         ) {
-            total += value
-            println(value)
+            total += value + 1
+            val ssx = sx?.toString() ?: " "
+            val sdx = dx?.toString() ?: " "
+            val sdw = dw?.toString() ?: " "
+            val sup = up?.toString() ?: " "
+
+            println(" $sup")
+            println("$ssx$value$sdx")
+            println(" $sdw")
+            println("##### $value -> $total")
         }
     }
     println("result $total")
