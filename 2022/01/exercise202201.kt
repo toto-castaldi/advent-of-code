73d285e06fa4012a3f30cc1925f525d2448f57c2
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-12-01 07:26:52 +0100

    2022.02

--- 2022/01/exercise202201.kt
+++ 2022/01/exercise202201.kt
@@ -4,21 +4,25 @@ import java.util.ArrayList
 fun main(args: Array<String>) {
     val fileName : String = args[0]
     val fileInput = File(fileName)
-    var maxCalories = 0
+    var elvesCalories: ArrayList<Int> = ArrayList()
     var currentCalories = 0
     fileInput.forEachLine {
         line ->
 
         if (line.trim().length == 0) {
+            elvesCalories.add(currentCalories)
             currentCalories = 0
         } else {
             currentCalories += line.trim().toInt()
         }
-        if (currentCalories > maxCalories) {
-            maxCalories = currentCalories
-        }
+
     }
-    println(maxCalories)
 
-
+    var caloriesSorted = elvesCalories.sortedDescending()
+    val first = caloriesSorted.get(0)
+    val second = caloriesSorted.get(1)
+    val thirth = caloriesSorted.get(2)
+    val total = first + second + thirth
+    println("result1 $first")
+    println("result2 $total")
 }
