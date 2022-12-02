2ffeb6aeaa40dc83d95e86d86b0fac6edafe0013
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-12-02 11:10:38 +0100

    2022.02 refactoring

--- 2022/02/exercise202202.kt
+++ 2022/02/exercise202202.kt
@@ -1,30 +1,30 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    val symbolMap = mapOf("A" to "R", "B" to "P", "C" to "S", "X" to "R", "Y" to "P", "Z" to "S")
-    val playerResults = mapOf("RS" to 0, "SP" to 0, "PR" to 0, "SR" to 6, "PS" to 6, "RP" to 6, "SS" to 3, "PP" to 3, "RR" to 3)
-    val chooseValues = mapOf("R" to 1, "P" to 2, "S" to 3)
+    val symbols = mapOf("A" to "R", "B" to "P", "C" to "S", "X" to "R", "Y" to "P", "Z" to "S")
+    val playingPoints = mapOf("RS" to 0, "SP" to 0, "PR" to 0, "SR" to 6, "PS" to 6, "RP" to 6, "SS" to 3, "PP" to 3, "RR" to 3)
+    val choosingPoints = mapOf("R" to 1, "P" to 2, "S" to 3)
     val winningMoves = mapOf("P" to "S", "R" to "P", "S" to "R")
-    val losingMoves = mapOf("S" to "P", "P" to "R", "R" to "S")
+    val losingMoves = winningMoves.entries.associate{(k,v)-> v to k}
     var totalScore1 = 0
     var totalScore2 = 0
 
     File(args[0]).forEachLine { line ->
         val (handFirst, handSecond) = line.trim().split(" ")
-        val first = symbolMap[handFirst]
-        var second = symbolMap[handSecond]
+        val first = symbols[handFirst]
+        var second = symbols[handSecond]
 
-        totalScore1 += chooseValues[second]!! + playerResults["$first$second"]!!
+        totalScore1 += choosingPoints[second]!! + playingPoints["$first$second"]!!
 
-        when (handSecond) {
-            "X" -> second = losingMoves[first]
-            "Z" -> second = winningMoves[first]
+        second = when (handSecond) {
+            "X" -> losingMoves[first]
+            "Z" -> winningMoves[first]
             else -> {
-                second = first
+                first
             }
         }
 
-        totalScore2 += chooseValues[second]!! + playerResults["$first$second"]!!
+        totalScore2 += choosingPoints[second]!! + playingPoints["$first$second"]!!
     }
 
     println("result1 $totalScore1")
