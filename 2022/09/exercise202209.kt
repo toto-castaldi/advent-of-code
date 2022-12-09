c63ff832a0141ec8156d65cc226defea9825dc05
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-12-09 17:06:49 +0100

    useless variable

--- 2022/09/exercise202209.kt
+++ 2022/09/exercise202209.kt
@@ -23,7 +23,6 @@ private fun part(fileName : String , len : Int) : Int{
         val steps = stepsString.toInt()
 
         for (i in 0 until steps) {
-            //move head
             val headCoordinates = rope.first()
             when (dir) {
                 "R" -> headCoordinates.move(1, 0)
@@ -31,19 +30,18 @@ private fun part(fileName : String , len : Int) : Int{
                 "L" -> headCoordinates.move(-1, 0)
                 "D" -> headCoordinates.move(0, 1)
             }
-            var lastTailPos = headCoordinates
             for (j in 0 until rope.size - 1) {
                 val first = rope[j]
                 val second = rope[j + 1]
                 val distance = first - second
-                val x  = if (abs(distance.y) > 1 || abs(distance.x) > 1) sign(distance.x.toDouble()).toInt() else 0
-                val y  = if (abs(distance.y) > 1 || abs(distance.x) > 1) sign(distance.y.toDouble()).toInt() else 0
+                val x = if (abs(distance.y) > 1 || abs(distance.x) > 1) sign(distance.x.toDouble()).toInt() else 0
+                val y = if (abs(distance.y) > 1 || abs(distance.x) > 1) sign(distance.y.toDouble()).toInt() else 0
 
-                lastTailPos = second.clone()
+                if (j == rope.size - 2) {
+                    tailPassing.add(rope.last().clone())
+                }
                 second.move(x, y)
             }
-
-            tailPassing.add(lastTailPos)
         }
     }
     tailPassing.add(rope.last())
