20dbb13b47188c395ee0ab6b440e1c76db40c039
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-12-09 15:18:31 +0100

    with set

--- 2022/09/exercise202209.kt
+++ 2022/09/exercise202209.kt
@@ -6,14 +6,14 @@ import kotlin.math.sign
 fun main(
     args: Array<String>
 ) {
-    part(args[0], 2)
-    part(args[0], 12)
-
+    println(part(args[0], 2))
+    println(part(args[0], 10))
 }
 
-private fun part(fileName : String , len : Int) {
-    val tailPassing = mutableMapOf<Coordinates, Int>()
-    tailPassing[Coordinates(0, 0)] = 1
+private fun part(fileName : String , len : Int) : Int{
+    val tailPassing = mutableSetOf<Coordinates>()
+
+    tailPassing.add(Coordinates(0, 0))
 
     val rope = (1..len).map { Coordinates(0, 0) }.toMutableList()
 
@@ -24,40 +24,29 @@ private fun part(fileName : String , len : Int) {
 
         for (i in 0 until steps) {
             //move head
-            val headCoordinates = rope[0]
+            val headCoordinates = rope.first()
             when (dir) {
                 "R" -> headCoordinates.move(1, 0)
                 "U" -> headCoordinates.move(0, -1)
                 "L" -> headCoordinates.move(-1, 0)
                 "D" -> headCoordinates.move(0, 1)
             }
-            var lastTailPos = rope[0]
+            var lastTailPos = headCoordinates
             for (j in 0 until rope.size - 1) {
                 val first = rope[j]
                 val second = rope[j + 1]
                 val distance = first - second
+                val x  = if (abs(distance.y) > 1 || abs(distance.x) > 1) sign(distance.x.toDouble()).toInt() else 0
+                val y  = if (abs(distance.y) > 1 || abs(distance.x) > 1) sign(distance.y.toDouble()).toInt() else 0
 
-                var x  = 0
-                var y  = 0
-
-                if (distance.x != 0 || distance.y != 0) {
-                    if (distance.y == 0 && abs(distance.x) > 1) x = sign(distance.x.toDouble()).toInt()
-                    else if (distance.x == 0 && abs(distance.y) > 1) y = sign(distance.y.toDouble()).toInt()
-                    else if (abs(distance.y) > 1 || abs(distance.x) > 1) {
-                        x = sign(distance.x.toDouble()).toInt()
-                        y = sign(distance.y.toDouble()).toInt()
-                    }
-                }
                 lastTailPos = second.clone()
-                second.move(x, y )
+                second.move(x, y)
             }
 
-            tailPassing[lastTailPos] = tailPassing[lastTailPos] ?.let { it + 1 } ?: 1
-
+            tailPassing.add(lastTailPos)
         }
     }
-    val tail = rope[rope.size - 1]
-    tailPassing[tail] = if (tail in tailPassing.keys) tailPassing[tail]!! + 1 else 1
+    tailPassing.add(rope.last())
 
-    println(tailPassing.keys.size)
+    return tailPassing.size
 }
