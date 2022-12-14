aeb51929c2377be664b5e4f701819c62af016548
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-12-14 18:07:27 +0100

    hill climbing wip

--- src/main/kotlin/com/toto_castaldi/common/algo/DiscreteHillClimbing.kt
+++ src/main/kotlin/com/toto_castaldi/common/algo/DiscreteHillClimbing.kt
@@ -2,11 +2,47 @@ package com.toto_castaldi.common.algo
 
 import com.toto_castaldi.common.structure.BidimentionalNode
 
-class DiscreteHillClimbing<T>(private val startingHill: BidimentionalNode<T>) {
+/**
+ * https://en.wikipedia.org/wiki/Hill_climbing
+ *
+ * Pseudocode
+ * algorithm Discrete Space Hill Climbing is
+ *     currentNode := startNode
+ *     loop do
+ *         L := NEIGHBORS(currentNode)
+ *         nextEval := −INF
+ *         nextNode := NULL
+ *         for all x in L do
+ *             if EVAL(x) > nextEval then
+ *                 nextNode := x
+ *                 nextEval := EVAL(x)
+ *         if nextEval ≤ EVAL(currentNode) then
+ *             // Return current node since no better neighbors exist
+ *             return currentNode
+ *         currentNode := nextNode
+ *
+ */
+class DiscreteHillClimbing<T>(private val startNode: BidimentionalNode<T>) {
 
-    fun compute(): List<T> {
-        println(startingHill)
-        TODO("compute")
+    fun compute(destination: BidimentionalNode<T>, EVAL : (BidimentionalNode<T>) -> Int): List<BidimentionalNode<T>> {
+        val result = mutableListOf<BidimentionalNode<T>>()
+        var currenNode = startNode
+        while (currenNode != destination) {
+            val L = currenNode.neighbors()
+            var nextEval = Int.MIN_VALUE
+            var nextNode : BidimentionalNode<T>? = null
+            for (x in L) {
+                if (EVAL(x) > nextEval) {
+                    nextNode = x
+                    nextEval = EVAL(x)
+                }
+            }
+            if (nextEval <= EVAL(currenNode)) {
+                result.add(currenNode)
+            }
+            currenNode = nextNode!!
+        }
+        return result
     }
 
 }
--- src/test/kotlin/com/toto_castaldi/common/algo/DiscreteHillClimbingTest.kt
+++ src/test/kotlin/com/toto_castaldi/common/algo/DiscreteHillClimbingTest.kt
@@ -1,6 +1,8 @@
 package com.toto_castaldi.common.algo
 
 import com.toto_castaldi.common.structure.BidimentionalNode
+import java.lang.Math.pow
+import kotlin.math.sqrt
 import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -8,17 +10,26 @@ import kotlin.test.assertEquals
 class DiscreteHillClimbingTest {
 
     @Test
-    @Ignore
     fun testSlope() {
-        val startingHill = BidimentionalNode(1)
+        val startingHill = BidimentionalNode(DistanceAndHeight(1,1))
         var pointer = startingHill
         for (i in 2..100) {
-            val next = BidimentionalNode(i)
+            val next = BidimentionalNode(DistanceAndHeight(i,i))
             pointer.neighbor(1, 0, next)
             pointer = next
         }
-        val path = DiscreteHillClimbing(startingHill).compute()
+
+        val path = DiscreteHillClimbing(startingHill).compute(pointer) {
+            sqrt(
+                pow(pointer.data.distance.toDouble() - it.data.distance.toDouble(), 2.toDouble()) +
+                   pow(pointer.data.height.toDouble() - it.data.height.toDouble(), 2.toDouble())
+            ).toInt()
+        }
         assertEquals(100, path.size)
+
+    }
+
+    data class DistanceAndHeight(val distance: Int, val height: Int) {
 
     }
 }
