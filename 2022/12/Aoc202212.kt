1b6e4435560f5e982e37e6ec54a233b99f13b5b4
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2023-01-10 18:29:24 +0100

    202212.2

--- 2022/12/Aoc202212.kt
+++ 2022/12/Aoc202212.kt
@@ -1,143 +1,53 @@
 import com.toto_castaldi.common.algo.Dijkstra
 import com.toto_castaldi.common.structure.BidimentionalNode
 import com.toto_castaldi.common.structure.Graph
-import com.toto_castaldi.common.structure.IntCoordinates
 import java.io.File
 
 class Aoc202212 () {
 
+    private lateinit var topLeft: BidimentionalNode<XYH>
+    private lateinit var dijkstra: Dijkstra<XYH>
+    private lateinit var elementS: XYH
+    private lateinit var elementE: XYH
     private val lines = mutableListOf<List<Char>>()
 
     val heightVal = { supply: Char -> supply.code - 'a'.code + 1 }
 
     data class XYH(val x: Int, val y: Int, val h : Int)
 
-    fun run() {
-        val heightmapInputChar = File("").readLines().map { line -> line.toCharArray().toList() }
-        var startCoordinates = IntCoordinates(0, 0)
-        var endCoordinates = IntCoordinates(0, 0)
-        val heightmapInput = mutableListOf<MutableList<Int>>()
-        for ((indexRow, row) in heightmapInputChar.withIndex()) {
-            val rowInt = mutableListOf<Int>()
-            for ((indexCol, element) in row.withIndex()) {
-                when (element) {
-                    'S' -> {
-                        startCoordinates = IntCoordinates(indexCol, indexRow)
-                        rowInt.add(heightVal('a'))
-                    }
-                    'E' -> {
-                        endCoordinates = IntCoordinates(indexCol, indexRow)
-                        rowInt.add(heightVal('z'))
-                    }
-                    else -> {
-                        rowInt.add(heightVal(element))
-                    }
-                }
-            }
-            heightmapInput.add(rowInt)
-        }
-        val (lastNode, _) = BidimentionalNode.build(heightmapInput) { x,y,h -> XYH(x,y,h) }
-        val firstNode = lastNode!!.topLeft()
-        var destination : BidimentionalNode<XYH>? = null
-        BidimentionalNode.printNodes(firstNode) {
-            it.h.toString().padStart(3)
-        }
-        val allNodes = mutableListOf<BidimentionalNode<XYH>>()
-        BidimentionalNode.navigate(firstNode) {
-            allNodes.add(it)
-            if (it.data.x == endCoordinates.x && it.data.y == endCoordinates.y) destination = it
-        }
-
-        for (node in allNodes) {
-            val u = node.u()
-            val d = node.d()
-            val l = node.l()
-            val r = node.r()
-            if (u != null && u.data.h > node.data.h + 1) node.removeNeighbor(0, -1)
-            if (d != null && d.data.h > node.data.h + 1) node.removeNeighbor(0, 1)
-            if (l != null && l.data.h > node.data.h + 1) node.removeNeighbor(-1, 0)
-            if (r != null && r.data.h > node.data.h + 1) node.removeNeighbor(1, 0)
-            node
-                .removeNeighbor(1, -1)
-                .removeNeighbor(1, 1)
-                .removeNeighbor(-1, 1)
-                .removeNeighbor(-1, -1)
-        }
-
-        println(destination)
-        println(startCoordinates)
-
-        TODO()
-    }
-
     operator fun plus(line: String) {
         lines.add(line.toList())
     }
 
     fun shortestPath(): Int {
-        var start = XYH(0,0,0)
-        var end = XYH(0,0,0)
-        val graph = Graph<XYH>()
-
-        val topLeft = BidimentionalNode.build(lines) {
-            x, y, c ->
-
-            val n = when (c) {
-                'S' -> {
-                    start = XYH(x, y, heightVal('a'))
-                    start
-                }
-                'E' -> {
-                    end = XYH(x, y, heightVal('z'))
-                    end
-                }
-                else -> {
-                    XYH(x, y, heightVal(c))
-                }
-            }
-
-            graph + n
-
-            n
-        }.node!!.topLeft()
-
-        BidimentionalNode.printNodes(topLeft) {
-            it.h.toString().padStart(3, ' ')
-        }
-
-        val dijkstra = Dijkstra<XYH>(graph)
-
+        init()
         BidimentionalNode.navigate(topLeft) {
-            from ->
+                from ->
             val valid = { f: XYH, d: XYH -> f.h + 1 >= d.h }
             from.u() ?.let { dest -> if (valid(from.data, dest.data)) dijkstra.oneWay(from.data, dest.data, 1)}
             from.d() ?.let { dest -> if (valid(from.data, dest.data)) dijkstra.oneWay(from.data, dest.data, 1)}
             from.l() ?.let { dest -> if (valid(from.data, dest.data)) dijkstra.oneWay(from.data, dest.data, 1)}
             from.r() ?.let { dest -> if (valid(from.data, dest.data)) dijkstra.oneWay(from.data, dest.data, 1)}
         }
+        println("${elementS.x} ${elementS.y}")
+        println("${elementE.x} ${elementE.y}")
 
-        println("${start.x} ${start.y}")
-        println("${end.x} ${end.y}")
-
-        return dijkstra.shortestFrom(start)[end]!!
+        return dijkstra.shortestFrom(elementS)[elementE]!!
     }
 
-    fun fewestShortestPath(): Int {
-        var start = XYH(0,0,0)
-        var end = XYH(0,0,0)
+    private fun init() {
         val graph = Graph<XYH>()
-
-        val topLeft = BidimentionalNode.build(lines) {
-                x, y, c ->
+        topLeft = BidimentionalNode.build(lines) {
+            x, y, c ->
 
             val n = when (c) {
                 'S' -> {
-                    end = XYH(x, y, heightVal('a'))
-                    end
+                    elementS = XYH(x, y, heightVal('a'))
+                    elementS
                 }
                 'E' -> {
-                    start = XYH(x, y, heightVal('z'))
-                    start
+                    elementE = XYH(x, y, heightVal('z'))
+                    elementE
                 }
                 else -> {
                     XYH(x, y, heightVal(c))
@@ -153,7 +63,13 @@ class Aoc202212 () {
             it.h.toString().padStart(3, ' ')
         }
 
-        val dijkstra = Dijkstra<XYH>(graph)
+        dijkstra = Dijkstra<XYH>(graph)
+
+
+    }
+
+    fun fewestShortestPath(): Int {
+        init()
 
         BidimentionalNode.navigate(topLeft) {
                 from ->
@@ -164,10 +80,10 @@ class Aoc202212 () {
             from.r() ?.let { dest -> if (valid(from.data, dest.data)) dijkstra.oneWay(from.data, dest.data, 1)}
         }
 
-        println("${start.x} ${start.y}")
-        println("${end.x} ${end.y}")
+        println("${elementE.x} ${elementE.y}")
+        println("${elementS.x} ${elementS.y}")
 
-        return dijkstra.shortestFrom(start).filter {
+        return dijkstra.shortestFrom(elementE).filter {
             entry ->
 
             entry.key.h == 1
--- README.md
+++ README.md
@@ -77,4 +77,4 @@ Tested with Kotlink version 1.7.21 and Java openjdk 17.0.3 2022-04-19
 
 ![2021 Calendar](./2021.12.1-calendar.png?&raw=true "2021 Calendar")
 
-![2022 Calendar](./2022.12.1-calendar.png?&raw=true "2022 Calendar")
+![2022 Calendar](./2022.12.2-calendar.png?&raw=true "2022 Calendar")
