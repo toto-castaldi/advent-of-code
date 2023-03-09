c20862b70d28db5ab72898fc0d090b9d0bf7fd5e
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2023-03-09 17:28:09 +0100

    2022.24 wip

--- 2022/24/Aoc202224.kt
+++ 2022/24/Aoc202224.kt
@@ -1,23 +1,38 @@
 
 import com.toto_castaldi.common.CrossDirection
+import com.toto_castaldi.common.WindRose
 import com.toto_castaldi.common.structure.IntCoordinates
+import com.toto_castaldi.common.structure.Matrix2D
 import java.io.File
 
 class Aoc202224() {
 
-    private val winds = mutableSetOf<IntCoordinates>()
+    private var w: Int = 0
+    private var h: Int = 0
+    private var winds = mutableSetOf<Wind>()
     private var walls = mutableSetOf<IntCoordinates>()
-    private var terrain = mutableSetOf<IntCoordinates>()
     private lateinit var start:IntCoordinates
     private lateinit var end:IntCoordinates
+    private lateinit var player:IntCoordinates
 
-    fun map(y: Int, height: Int, elvesPos: String) {
-        for ((x, c) in elvesPos.toCharArray().toList().withIndex()) {
+    fun map(y: Int, height: Int, mapLine: String) {
+        h = height
+        w = mapLine.length
+        for ((x, c) in mapLine.toCharArray().toList().withIndex()) {
             val p = IntCoordinates(x,y)
             when (c) {
                 '#' -> walls.add(p)
-                '.' -> if (y == 0) start = p else if (y + 1 == height) end = p else terrain.add(p)
+                '.' -> if (y == 0) {
+                    start = p
+                    walls.add(p)
+                } else if (y + 1 == height) {
+                    end = p
+                    walls.add(p)
+                }
                 '^' -> winds.add(Wind(CrossDirection.U, IntCoordinates(x,y)))
+                'v' -> winds.add(Wind(CrossDirection.D, IntCoordinates(x,y)))
+                '<' -> winds.add(Wind(CrossDirection.L, IntCoordinates(x,y)))
+                '>' -> winds.add(Wind(CrossDirection.R, IntCoordinates(x,y)))
             }
 
         }
@@ -25,8 +40,79 @@ class Aoc202224() {
 
     data class Wind(val crossDirection: CrossDirection,val coordinates: IntCoordinates)
 
-    fun stepToExit(): Int {
-        TODO("Not yet implemented")
+    fun stepToExit(debug:Int = -1): Int {
+        player = start
+        var steps = 0
+
+        while (player != end && (debug == -1 || steps < debug)) {
+            debug()
+
+            val newWinds = mutableSetOf<Wind>()
+            for (wind in winds) {
+                when (wind.crossDirection) {
+                    CrossDirection.U -> {
+                        var next = WindRose.n(wind.coordinates)
+                        if (next in walls) {
+                            next = IntCoordinates(next.x, h - 2)
+                        }
+                        newWinds.add(Wind(wind.crossDirection, next))
+                    }
+                    CrossDirection.D -> {
+                        var next = WindRose.s(wind.coordinates)
+                        if (next in walls) {
+                            next = IntCoordinates(next.x, 1)
+                        }
+                        newWinds.add(Wind(wind.crossDirection, next))
+                    }
+                    CrossDirection.R -> {
+                        var next = WindRose.e(wind.coordinates)
+                        if (next in walls) {
+                            next = IntCoordinates(1, next.y)
+                        }
+                        newWinds.add(Wind(wind.crossDirection, next))
+                    }
+                    CrossDirection.L -> {
+                        var next = WindRose.w(wind.coordinates)
+                        if (next in walls) {
+                            next = IntCoordinates(w - 2, next.y)
+                        }
+                        newWinds.add(Wind(wind.crossDirection, next))
+                    }
+                }
+            }
+            winds = newWinds
+
+
+
+            steps += 1
+        }
+
+        return steps
+    }
+
+    private fun debug() {
+        val m = Matrix2D(w, h, '.')
+
+        for (c in winds) {
+            m[c.coordinates.x, c.coordinates.y] = when (c.crossDirection) {
+                CrossDirection.U -> '^'
+                CrossDirection.D -> 'v'
+                CrossDirection.L -> '<'
+                CrossDirection.R -> '>'
+            }
+        }
+
+        for (c in walls) {
+            m[c.x, c.y] = '#'
+        }
+
+        m[start.x, start.y] = '.'
+
+        m[end.x, end.y] = '.'
+
+        m[player.x, player.y] = 'E'
+
+        println(m)
     }
 
     companion object {
