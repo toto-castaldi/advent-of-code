1d13d32a8a2e6407a89e687fb718d43a4f31bf44
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2025-08-22 11:15:51 +0200

    2023.4.2 wip

--- 2023/04/202304.ts
+++ 2023/04/202304.ts
@@ -1,30 +1,56 @@
 import { intersection, readInputLines } from '../../src/main/typescript/utils.ts';
 
+type CardsInfo = {
+  [id: number]: number;
+};
+
 export class ScrathCards {
 
-    private sumOfCardPoints : number;
+    private cardsInfo : CardsInfo;
 
     constructor() {
-        this.sumOfCardPoints = 0;
+      this.cardsInfo = {};
     }
 
     public addCard(line: string) : void {
-        const numberLines = line.split(":")[1].split("|");
-        const winningNumbers = [...new Set(numberLines[0].trim().split(" ").filter(s=> s.trim().length > 0).map(s => parseInt(s.trim())))];
-        const havingNumbers = [...new Set(numberLines[1].trim().split(" ").filter(s=> s.trim().length > 0).map((s) => parseInt(s.trim())))];
+      const idNumbers = line.split(":").map(s => s.trim());
+      const numberLines = idNumbers[1].split("|");
+      const id : number = parseInt(idNumbers[0].split("Card ")[1].trim());
+      const winningNumbers = [...new Set(numberLines[0].trim().split(" ").filter(s=> s.trim().length > 0).map(s => parseInt(s.trim())))];
+      const havingNumbers = [...new Set(numberLines[1].trim().split(" ").filter(s=> s.trim().length > 0).map((s) => parseInt(s.trim())))];
 
-        const i = intersection(winningNumbers, havingNumbers);
+      const i = intersection(winningNumbers, havingNumbers);
 
-        //console.log(line, winningNumbers, havingNumbers, i);
+      //console.log(line, winningNumbers, havingNumbers, i);
 
-        if (i.length > 0) {
-            this.sumOfCardPoints += Math.pow(2, i.length - 1);
-        }
-
+      this.cardsInfo[id] = i.length;
+      
     }
 
+    private cardPoints( winningNumberCounts : number) : number {
+      return winningNumberCounts > 0 ? Math.pow(2, winningNumberCounts - 1) : 0;
+    } 
+
     public getSumOfCardPoints() : number {
-        return this.sumOfCardPoints;
+      return Object.values(this.cardsInfo).reduce((acc, val) => {
+        return acc + this.cardPoints(val);
+      }, 0);
+    }
+
+    public getSumOfCardPointsWithBonus() : number {
+      return Object.entries(this.cardsInfo).reduce((acc, [id, val]) => {
+        let s = acc + val;
+        if (val > 0) {
+          console.log(`points for Card ${id}`);  
+          for (let index = 0; index < val; index ++) {
+            const i = parseInt(id) + index + 1;
+            const p = this.cardPoints(parseInt(id) + index + 1);
+            console.log(`adding points for Card ${i}`);
+            s += p;
+          } 
+        };
+        return s;
+      }, 0);
     }
     
 }
