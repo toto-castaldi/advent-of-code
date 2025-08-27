830da37a1427db1fc4e95ca703f99e04470b61cd
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2025-08-27 09:33:40 +0200

    2023.06 wip

---
+++ 2023/06/07/202306.test.ts
@@ -0,0 +1,16 @@
+import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
+import { BoatRace } from './202306.ts';
+
+Deno.test("step one", () => {
+  const boatRace = new BoatRace();
+  boatRace.debug = true;
+  
+  boatRace.addRace(7,9);
+  boatRace.addRace(15,40);
+  boatRace.addRace(30,200);
+  
+  assertEquals(boatRace.winningWayCount(0), 4);
+  assertEquals(boatRace.winningWayCount(1), 8);
+  assertEquals(boatRace.winningWayCount(2), 9);
+
+});
