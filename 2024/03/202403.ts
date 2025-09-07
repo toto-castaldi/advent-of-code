6e12dea545133fd14c3d5d9c1569023991ac5e22
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2025-09-07 12:20:01 +0200

    2024.03.2 refactoring

--- 2024/03/202403.ts
+++ 2024/03/202403.ts
@@ -1,19 +1,77 @@
 import { readInputLines } from '../../src/main/typescript/utils.ts';
 
+enum MatchType {
+  do,
+  dont,
+  mul
+}
+
+interface Match {
+  matchType: MatchType;
+  position: number;
+  length: number;
+  match: string;
+  x : number | null,
+  y : number | null
+}
+
 export class MulComputer {
 
     public debug = false;
     private multiplications : Array<{a: number, b: number, result: number}> = []; 
     
     add(line: string) : void {
-      const regex = /mul\((\d+),(\d+)\)/g;
-      const matches = [...line.matchAll(regex)];
-    
-      this.multiplications.push(...matches.map(match => ({
-        a: parseInt(match[1]),
-        b: parseInt(match[2]),
-        result: parseInt(match[1]) * parseInt(match[2])
+      const matches: Match[] = [];
+  
+      const doPattern = /do\(\)/g;
+      let match;
+      
+      while ((match = doPattern.exec(line)) !== null) {
+        matches.push({
+          matchType: MatchType.do,
+          position: match.index,
+          length: match[0].length,
+          match: match[0],
+          x : null,
+          y : null
+        });
+      }
+      
+      const dontPattern = /don't\(\)/g;
+      
+      while ((match = dontPattern.exec(line)) !== null) {
+        matches.push({
+          matchType: MatchType.dont,
+          position: match.index,
+          length: match[0].length,
+          match: match[0],
+          x : null,
+          y : null
+        });
+      }
+      
+      const mulPattern = /mul\((\d+),(\d+)\)/g;
+      
+      while ((match = mulPattern.exec(line)) !== null) {
+        matches.push({
+          matchType: MatchType.mul,
+          position: match.index,
+          length: match[0].length,
+          match: match[0],
+          x : parseInt(match[1]),
+          y : parseInt(match[2])
+        });
+      }
+      
+      matches.sort((a, b) => a.position - b.position);
+
+      this.multiplications.push(...matches.filter(m => m.matchType === MatchType.mul).map(m => ({
+        a: m.x!,
+        b: m.y!,
+        result: m.x! * m.y!
       })));
+
+      
     }
 
     sumOfMultiplication(): number {
